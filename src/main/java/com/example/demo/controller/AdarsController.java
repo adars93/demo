a126a4ed76c298d777df4d6822da0179a4caa8ca
Adars Sonar <adars.sonar@capgemini,com>
2024-07-04 16:52:53 +0530

    AdarsController created

--- src/main/java/com/example/demo/controller/AdarsController.java
+++ src/main/java/com/example/demo/controller/AdarsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdarsController {
 
-    @GetMapping("/hello")
+    @GetMapping("/helloAdars")
     public String sayHello() {
         return "Hello Adars";
     }
