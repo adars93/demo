d97b61d9d4010095ee7ad3c28286fa4acb93fcf8
shubdas <shubham.a.das@capgemini.com>
2024-07-04 17:18:56 +0530

    shubham controller upgraded

--- src/main/java/com/example/demo/controller/ShubhamController.java
+++ src/main/java/com/example/demo/controller/ShubhamController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ShubhamController {
-    @GetMapping("/helloshubham")
+    @GetMapping("/helloShubham")
     public String sayHello() {
         return "Hello from Shubham";
     }
